48b173b8d692364e672d469ed96d5a893ff2257f
phatcharamani ketkhuang <1phatchara11@gmail.com>
2022-03-07 11:33:33 +0700

    first commit

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -2,28 +2,79 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
+        <ion-title>ข่าวสาร</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
+      <ion-card v-for="(news, i) in this.dataNews" :key="i">
+        <img :src="news.urlToImage" />
+        <ion-item>
+          <ion-icon :icon="pin" slot="start"></ion-icon>
+          <ion-label>{{ news.title }}</ion-label>
+          <ion-button :href="news.url" fill="outline" slot="end"
+            >View</ion-button
+          >
+        </ion-item>
+
+        <ion-card-content>
+          {{ news.description }}
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
+import {
+  IonCard,
+  IonCardContent,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+} from "@ionic/vue";
+import { pin } from "ionicons/icons";
+import axios from "axios";
 
-export default defineComponent({
-  name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-});
+export default {
+  name: "Tab2Page",
+  components: {
+    IonCard,
+    IonCardContent,
+    IonIcon,
+    IonItem,
+    IonLabel,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+    IonButton,
+  },
+  setup() {
+    return { pin };
+  },
+  data() {
+    return {
+      dataNews: null,
+    };
+  },
+  mounted() {
+    this.loadNews();
+  },
+  methods: {
+    async loadNews() {
+      const res = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=th&apiKey=3ca8bdceff3a4d309c60fdfa94f37b08"
+      );
+      this.dataNews = res.data.articles;
+      console.log(this.dataNews);
+    },
+  },
+};
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -17,6 +17,12 @@
           <ion-icon :icon="square" />
           <ion-label>Tab 3</ion-label>
         </ion-tab-button>
+
+        <ion-tab-button tab="tab4" href="/tabs/tab4">
+          <ion-icon :icon="home" />
+          <ion-label>Tab 4</ion-label>
+        </ion-tab-button>
+        
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -25,7 +31,7 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle, home } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
@@ -35,6 +41,7 @@ export default defineComponent({
       ellipse, 
       square, 
       triangle,
+      home,
     }
   }
 });
